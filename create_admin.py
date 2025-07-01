07b61c3801b3f3cfbddadb6c8a0dafd0f567eb6b
Nikolay <104066410+crowstrong@users.noreply.github.com>
2025-07-01 18:26:53 +0200

    Mobile adap design added

--- create_admin.py
+++ create_admin.py
@@ -9,11 +9,16 @@ username = os.getenv("ADMIN_USERNAME")
 password = os.getenv("ADMIN_PASSWORD")
 
 with app.app_context():
-    if AdminUser.query.filter_by(username=username).first():
-        print(f"Admin user '{username}' already exists.")
-    else:
-        admin = AdminUser(username=username)
-        admin.set_password(password)
-        db.session.add(admin)
-        db.session.commit()
-        print(f"Admin user '{username}' created successfully.")
+    # Drop all prev accounts
+    print("Deleting all existing admin users...")
+    AdminUser.query.delete()
+    db.session.commit()
+    print("All existing admin users deleted.")
+
+    # Create new admin
+    print(f"Creating admin user '{username}'...")
+    admin = AdminUser(username=username)
+    admin.set_password(password)
+    db.session.add(admin)
+    db.session.commit()
+    print(f"Admin user '{username}' created successfully.")
